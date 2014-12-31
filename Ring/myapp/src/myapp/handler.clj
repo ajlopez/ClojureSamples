606949718e9688bae52dc7d740239b3733739864
ajlopez <webmaster@ajlopez.com>
2014-12-31 17:10:39 -0300

    Adding wrap-reload to app

--- Ring/myapp/src/myapp/handler.clj
+++ Ring/myapp/src/myapp/handler.clj
@@ -1,5 +1,6 @@
 (ns myapp.handler
-  (:use ring.adapter.jetty))
+  (:use ring.adapter.jetty)
+  (:use ring.middleware.reload))
 
 (defn init []
   (println "myapp is starting"))
@@ -7,10 +8,13 @@
 (defn destroy []
   (println "myapp is shutting down"))
 
-(defn app [req]
+(defn handler [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    "Hello World from Ring"})
+
+(def app 
+    (wrap-reload handler))
 
 (defn boot []
   (run-jetty #'app {:port 3000}))
