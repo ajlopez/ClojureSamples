b7e91d43b26016fc06d84330fb874fcc303dd56f
ajlopez <webmaster@ajlopez.com>
2017-10-22 14:26:49 -0300

    helloring sample with plugin

---
+++ Ring/helloring/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to helloring
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ Ring/helloring/test/helloring/core_test.clj
@@ -0,0 +1,7 @@
+(ns helloring.core-test
+  (:require [clojure.test :refer :all]
+            [helloring.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
